bd80b1263d597171253a8ba94bb8b6ba27c25106
Vishwaa <vish@gmail.com>
2024-02-19 09:14:18 +0530

    Initial commit

--- index.js
+++ index.js
@@ -5,33 +5,32 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 const contactRoutes = require('./routes/contactRoutes');
-require("dotenv").config({ path: "./.env" })
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URL)
+require("dotenv").config({ path: "./.env" });
+
+mongoose.connect(process.env.MONGO_URL);
 const app = express();
-app.use(express.json())
-app.use(express.static(path.join(__dirname, "dist")))
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(cors());
+app.use(express.json());
 
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true
-// }))
-app.use(cors())
 const PORT = process.env.PORT || 5000;
-
 
 app.use('/api/contact', contactRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: "resource Not found" });
+});
+
 app.use("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
-    res.status(404).json({ message: "resource Not foudn" })
-})
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.use((err, req, res, next) => {
     console.log(err);
-    res.status(500).json({ message: "something went wrong" })
-})
+    res.status(500).json({ message: "something went wrong" });
+});
 
 mongoose.connection.once("open", () => {
     console.log('MONGO CONNECTED');
-    app.listen(process.env.PORT, console.log("SERVER RUNNING"))
-})
+    app.listen(PORT, () => console.log("SERVER RUNNING"));
+});
