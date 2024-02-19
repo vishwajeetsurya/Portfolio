ceac099ac1983207134a7ae38b3879751a5e115f
Vishwaa <vish@gmail.com>
2024-02-19 08:25:18 +0530

    Initial commit

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ require("dotenv").config({ path: "./.env" })
 mongoose.connect(process.env.MONGO_URL)
 const app = express();
 app.use(express.json())
-// app.use(express.static(path.join(__dirname, "dist")))
+app.use(express.static(path.join(__dirname, "dist")))
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
 // app.use(cors({}))
 const PORT = process.env.PORT || 5000;
 
@@ -23,9 +23,9 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/contact', contactRoutes);
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resource Not Found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    res.status(404).json({ message: "resource Not foudn" })
 })
-
 app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).json({ message: "something went wrong" })
